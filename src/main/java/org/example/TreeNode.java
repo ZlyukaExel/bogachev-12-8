d960f63527a40a67ca1eba726b2362fc67494cc5
ZlyukaExel <126321708+ZlyukaExel@users.noreply.github.com>
2024-12-27 17:56:50 +0300

    djdkjdrkjdr

--- src/main/java/org/example/TreeNode.java
+++ src/main/java/org/example/TreeNode.java
@@ -1,97 +1,101 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class TreeNode {
-    private String value;
-    private List<TreeNode> children;
-    private static StringBuilder prefix = new StringBuilder();
+    private final String value;
+    private final List<TreeNode> children;
 
     public TreeNode(String value) {
         this.value = value;
         this.children = new ArrayList<>();
     }
 
-    public static TreeNode parseTree(String input) {
+    public static TreeNode makeTree(String input) {
+        //убираем лишние пробелы и скобки по бокам
         input = input.replace(" ", "");
+        input = input.substring(1, input.length() - 1);
 
+        //если элемент один, то проходить всю программу нет смысла
         if (input.contains(",")) {
-            for (int i = 0; i < input.length(); i++) {
-                char ch = input.charAt(i);
-                if (ch != ',')
-                    System.out.print(ch);
-                else {
-                    System.out.print("\n" + prefix.toString() + "+-");
-                    input = input.substring(i + 1);
-                    break;
+            String[] allElements = input.split(",");
+
+            //сохраняем корень дерева
+            TreeNode result = new TreeNode(allElements[0]);
+            String[] elements = Arrays.copyOfRange(allElements, 1, allElements.length);
+
+            //значение узла
+            StringBuilder val = new StringBuilder();
+            int bracketCounter = 0;
+
+            //проходим по каждому элементу
+            for (String element : elements) {
+                //если это узел, добавляем всё подряд
+                if (element.contains("(") || bracketCounter > 0) {
+                    for (char ch : element.toCharArray()) {
+                        val.append(ch);
+                        if (ch == '(')
+                            bracketCounter++;
+                        if (ch == ')')
+                            bracketCounter--;
+                        if (bracketCounter == 0) {
+                            result.children.add(makeTree(val.toString()));
+                            val.setLength(0);
+                        }
+                    }
+                    if (!val.isEmpty())
+                        val.append(',');
+                } else {
+                    result.children.add(new TreeNode(element));
                 }
             }
-        } else {
-            prefix.setLength(prefix.length() - 2);
-            System.out.print(input);
-            return null;
-        }
-        return parseSubtree(input);
+            return result;
+        } else
+            return new TreeNode(input);
     }
 
-    private static TreeNode parseSubtree(String input) {
-        StringBuilder newNode = new StringBuilder();
-        int bracketCounter = 0;
 
-        for (int i = 0; i < input.length(); i++) {
-            char ch = input.charAt(i);
-
-            //если это узел, добавляем всё подряд
-            if (bracketCounter > 0) {
-                //каждая ) понижает счётчик
-                if (ch == ')') {
-                    bracketCounter--;
-                    //если счётчик обнулился, строим дерево по полученной строке
-                    if (bracketCounter == 0) {
-                        //проверяем, есть ли после узла ещё элементы
-                        if (i + 1 < input.length() && input.charAt(i + 1) == ',')
-                            prefix.append("| ");
-                        else
-                            prefix.append("  ");
-                        parseTree(newNode.toString());
-                        newNode.setLength(0);
-                    } else
-                        newNode.append(ch); //сохраняем ) если узел не закончился
-                } else {
-                    //каждая ( повышает счётчик
-                    if (ch == '(')
-                        bracketCounter++;
-                    //сохраняем все остальные символы
-                    newNode.append(ch);
-                }
-            } else if (ch == ',') { //если началось новое слово, переходим к новой строке
-                System.out.print("\n" + prefix.toString() + "+-");
-            } else if (ch == '(') { //если начался узел, повышаем счётчик
-                bracketCounter++;
-            } else //если это обычный символ, выводим его на экран
-                System.out.print(ch);
+    public static StringBuilder prefix = new StringBuilder();
+/*
+    public static void PrintTree(TreeNode tree) {
+        System.out.println(tree.value);
+        for (TreeNode child : tree.children) {
+            prefix.setLength(0);
+            prefix.append("+-");
+            PrintChildren(child);
         }
-
-        if (!prefix.isEmpty())
-            prefix.setLength(prefix.length() - 2);
-
-        return null;
+        prefix.setLength(prefix.length() - 2);
     }
-
-    public static boolean isLast(String substring) {
-        return true;
+  */
+    public static void PrintChildren(TreeNode tree){
+        System.out.print(prefix.toString());
+        System.out.println(tree.value);
+        prefix.append("  ");
+        for (TreeNode child : tree.children) {
+            PrintChildren(child);
+        }
+        prefix.setLength(prefix.length() - 2);
     }
 
     public static void main(String[] args) {
         String input1 = "(a, b, (c), d)";
-        TreeNode root1 = parseTree(input1.substring(1, input1.length() - 1));
+        TreeNode root1 = makeTree(input1);
+        PrintTree(root1);
 
         System.out.println();
         System.out.println();
 
-        prefix.setLength(0);
         String input2 = "(a, second, (abc, y, (x, 7), uuu, (8, 9, (10, 1))), abcddcba)";
-        TreeNode root2 = parseTree(input2.substring(1, input2.length() - 1)); //без скобок
+        TreeNode root2 = makeTree(input2);
+        PrintTree(root2);
+
+        System.out.println();
+        System.out.println();
+
+        String input3 = "(a, (b, (c, (d, (e, (f, (g, (h, (i, (j, (k, (l, (m, (n, (o, (p))))))))))))))), z)";
+        TreeNode root3 = makeTree(input3);
+        PrintTree(root3);
     }
 }
